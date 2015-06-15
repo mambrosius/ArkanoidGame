7adbe1cc6770696bb4e848c39b2c958f7cdcc6cd
ambro <Ambro@Ambro-macbook.local>
2015-06-15 11:38:50 +0200

    Modified hardwareSetup

--- include/src/hardwareSetup.c
+++ include/src/hardwareSetup.c
@@ -1,26 +1,47 @@
 
+long ms = 0;
+long ball_timer = 0;
 
 #pragma interrupt
-void (){
+int timer0int() {
 
-void timersetup(){
+	ms++:
+	ball_timer++; 
+}
+
+void timerSetup(){
 
 	DI();
-	T0CTL = 0x39;
-	T0H = 0x00;
-	T0L = 0x01;
-	T0RH = 0x00;
-	T0RL = 0x48;
-	SET_VECTOR(TIMER0,timer0int);
-	
-	IRQ0ENH |= 0x00;
-	IRQ0ENL |= 0x20;
-	
-	
+	T0CTL = 0x09;	// Timer control
+	T0H   = 0x00;	// Timer high
+	T0L   = 0x01;	// Timer low
+	T0RH  = 0x24;	// Timer reload high
+	T0RL  = 0x00;	// Timer reload low
 
-	//start count
+	SET_VECTOR(TIMER0,timer0int);
+
+	IRQ0ENH |= 0x20; // Enable high
+	IRQ0ENL &= 0xDF; // Enable low 
+
+	// Starting count
 	T0CTL |= 0x80;
 
-    
 	EI();	
+}
+
+void keySetup(){
+
+	PFDD = 0xC0;
+	PDDD = 0x08;
+
+}
+
+char keyPress(){
+ 	
+ 	char r=0;
+ 	
+	if(((PDIN >> 3) & 1) == 0) r+=1;
+  	if(((PFIN >> 6) & 1) == 0) r+=2;
+
+  return r;
 }
